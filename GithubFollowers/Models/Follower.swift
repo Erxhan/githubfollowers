78b7a22225249c7b0120158869e994ba4bc1025e
Erxhan Selmani <ese@radionomy.com>
2020-01-02 08:22:40 +0100

    Models + Codable

---
+++ GithubFollowers/Models/Follower.swift
@@ -0,0 +1,14 @@
+//
+//  Follower.swift
+//  GithubFollowers
+//
+//  Created by Exrhan Selmani on 02/01/2020.
+//  Copyright © 2020 Exrhan Selmani. All rights reserved.
+//
+
+import Foundation
+
+struct Follower: Codable {
+    var login: String
+    var avatarUrl: String
+}
