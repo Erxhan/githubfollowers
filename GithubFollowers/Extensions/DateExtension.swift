d135246f541cb5900e53b469382a74ae3b7cad88
Erxhan Selmani <ese@radionomy.com>
2020-01-27 08:56:34 +0100

    Date Conversions and Date Label

---
+++ GithubFollowers/Extensions/DateExtension.swift
@@ -0,0 +1,17 @@
+//
+//  Date+Ext.swift
+//  GithubFollowers
+//
+//  Created by Exrhan Selmani on 27/01/2020.
+//  Copyright © 2020 Exrhan Selmani. All rights reserved.
+//
+
+import Foundation
+
+extension Date {
+    func convertToMonthYearFormat() -> String {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "MMM yyyy"
+        return dateFormatter.string(from: self)
+    }
+}
---
+++ GithubFollowers/Extensions/StringExtension.swift
@@ -0,0 +1,24 @@
+//
+//  StringExtension.swift
+//  GithubFollowers
+//
+//  Created by Exrhan Selmani on 27/01/2020.
+//  Copyright © 2020 Exrhan Selmani. All rights reserved.
+//
+
+import Foundation
+
+extension String {
+    func convertToDate() -> Date? {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ssZ"
+        dateFormatter.locale = .current
+        dateFormatter.timeZone = .current
+        return dateFormatter.date(from: self)
+    }
+    
+    func convertToDisplayFormat() -> String {
+        guard let date = self.convertToDate() else { return "N/A" }
+        return date.convertToMonthYearFormat()
+    }
+}
